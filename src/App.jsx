dc088a4f6fdd57243784b4a933da8550fc94ec98
bernard muthengi <bernardmuthengi9@gmail.com>
2026-09-17 15:34:38 +0300

    restored again

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 
+import WhatsAppButton from "./components/common/WhatsAppButton";
+import ScrollToTopButton from "./components/common/ScrollToTopButton";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
@@ -20,14 +23,10 @@ function App() {
 
       <main>
         <Routes>
-          {/* Main Pages */}
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
-
-          {/* Individual Service Pages */}
           <Route path="/services/:slug" element={<ServiceDetail />} />
-
           <Route path="/projects" element={<Projects />} />
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/contact" element={<Contact />} />
@@ -36,6 +35,10 @@ function App() {
       </main>
 
       <Footer />
+
+      {/* Floating Action Buttons */}
+      <WhatsAppButton />
+      <ScrollToTopButton />
     </>
   );
 }
